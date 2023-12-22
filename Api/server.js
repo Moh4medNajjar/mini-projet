ee088447f2e6f53b0b122f74d28973dfcff3d25d
Moh4medNajjar <najjarmohamed443@gmail.com>
2023-12-22 17:55:03 +0100

    fixed some issues in server.js

--- Api/server.js
+++ Api/server.js
@@ -9,16 +9,8 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/taskmanager', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
-// Define mongoose schemas and models for User, Comment, and Task here
 
-// Define routes for handling tasks, users, comments, etc.
-const taskRoutes = require('./routes/task.routes');
-app.use('/api/tasks', taskRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
