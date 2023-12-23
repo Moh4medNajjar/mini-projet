738456a6a71d87efcbd92e235751c9209f661cee
BEN AMOR Aziz <benamoraziz0220@gmail.com>
2023-12-23 19:50:03 +0100

    backend done

--- Api/server.js
+++ Api/server.js
@@ -8,9 +8,10 @@ app.use(express.json());
 const port = 3000;
 const userApi = require('./routes/user');
 const taskApi = require('./routes/task')
+const commentApi = require('./routes/comment')
 app.use('/user', userApi) 
 app.use('/task', taskApi) 
-
+app.use('/comment', commentApi) 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
