520c4dd85a3f9f91afc8a9bb13d1db69ba8040c1
Moh4medNajjar <najjarmohamed443@gmail.com>
2023-12-24 14:59:30 +0100

    implemented frontend login, register and dashboard pages; dashboard is not fully completed

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: DashboardComponent},
+  {path: "register", component: SignupComponent},
+  {path: "login", component: SigninComponent},
+];
